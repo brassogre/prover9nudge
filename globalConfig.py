62e9b36aeb003a9b85a40b53fff4e2cea19bca0c
Zachary Ernst <zernst@narrativescience.com>
2014-01-18 11:31:53 -0600

    Added a lot of docstrings

--- globalConfig.py
+++ globalConfig.py
@@ -1,35 +1,64 @@
+"""This class does nothing except keep all global configuration options. It is
+   imported by (I think) every module."""
+
 import os
 import sys
 import string
-import MySQLdb
+#import MySQLdb
+
+"""
+"""
 
 BASE_DIRECTORY = '/home/zac/projects/hintomatic/'
+""":ivar BASE_DIRECTORY: Where the hintomatic project lives"""
 TMP_DIRECTORY = '/tmp/'
-
+""":ivar TMP_DIRECTORY: Where temporary files will be created and destroyed"""
 EMAIL_ACCOUNT = 'hintomatic@gmail.com'
+""":ivar EMAIL_ACCOUNT: The hintomatic email address to which users will
+                     submit input and output files"""
 SMTP_SERVER = 'smtp.gmail.com'
+""":ivar SMTP_SERVER: Server for hintomatic email account (gmail)"""
 EMAIL_PASSWORD ='*********'
+""":ivar EMAIL_PASSWORD: Password for hintomatic email account"""
 IMAP_SERVER = 'imap.gmail.com'
+""":ivar IMAP_SERVER: The address for hintomatic's email account's IMAP"""
 MYSQL_DATABASE = 'autoprove'
+""":ivar MYSQL_DATABASE: Name of database for storing proofs, clauses, etc."""
 MYSQL_USER = '**********'
+""":ivar MYSQL_USER: User authorized for access to MYSQL"""
 MYSQL_PASSWORD = '*********'
-
+""":ivar MYSQL_PASSWORD: That user's password"""
 TEST_DIRECTORY = BASE_DIRECTORY + 'test_files/'
+""":ivar TEST_DIRECTORY: Where sample input and output files are kept"""
 TEST_INPUT_FILE = TEST_DIRECTORY + './prop.in'
-
+""":ivar TEST_INPUT_FILE: Relative pathname to a sample input file for testing"""
 SOS_START = 'formulas(sos)'
+""":ivar SOS_START: String that marks the beginning of the SOS list in a
+                 Prover9 file"""
 USABLE_START = 'formulas(usable)'
+""":ivar USABLE_START: String marking the beginning of the USABLE list in
+                 a Prover9 file"""
 GOALS_START = 'formulas(goals)'
+""":ivar GOALS_START: Marks the beginning of the GOALS list in a Prover9 file"""
 END_OF_LIST = 'end_of_list'
+""":ivar END_OF_LIST: String used to end any list in a Prover9 file"""
 COMMENT_MARKER = '%'
+""":ivar COMMENT_MARKER: Anything following this character is a comment for Prover9"""
 END_OF_STATEMENT = '.'
+""":ivar END_OF_STATEMENT: Delimiter marking the end of any Prover9 statement"""
 DISJUNCTION = '|'
+""":ivar DISJUNCTION: Separator for unit clauses in a disjunction"""
 SOS = 'sos'
+""":ivar SOS: Name of SOS list"""
 USABLE = 'usable'
+""":ivar USABLE: Name of Usable list"""
 GOALS = 'goals'
+""":ivar GOALS: Name of goals list"""
 SEARCH_STRING = '==== SEARCH ===='
-
+""":ivar SEARCH_STRING: Marks the beginning of the search phase for Prover9"""
 PROOF_START = '===== PROOF ====='
+""":ivar PROOF_START: String that marks the beginning of a proof in a Prover9
+                   output file"""
 PROOF_END = '==== end of proof ===='
 
 LYNX_COMMAND = '/usr/bin/lynx'
